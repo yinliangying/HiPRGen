2ce22358adba924fa5a60a6b7f7a0e1f28f62e71
ddd <542260219@qq.com>
2024-10-23 16:47:41 +0800

    修改logging.py文件名，不宜和python模块名相同， python /root/HiPRGen/HiPRGen/test.py 执行这类脚本import rdkit会有问题

--- MechFinder_rxn_filter_study/main.py
+++ MechFinder_rxn_filter_study/main.py
@@ -279,14 +279,15 @@ def apply_MechFinder(mapped_rxn_smarts_file: str, mech_output_file: str):
                 if "except" not in result_info_dict:
                     result_info_dict["except"]=0
                 result_info_dict["except"]+=1
-                continue
-
-            if MT_class not in result_info_dict:
-                result_info_dict[MT_class] = 0
-            result_info_dict[MT_class] += 1
-            if not isinstance(finder.check_exception(MT_class), str):
-                if MT_class!="mechanism not in collection":
-                    print(f"{rxn_id},{rxn_str},{updated_reaction},{LRT},{MT_class},{electron_path}",file=fp_out)
+                MT_class="except"
+            else:
+                if MT_class not in result_info_dict:
+                    result_info_dict[MT_class] = 0
+                result_info_dict[MT_class] += 1
+                if not isinstance(finder.check_exception(MT_class), str):
+                    if MT_class!="mechanism not in collection":
+                        print(f"{rxn_id},{rxn_str},{updated_reaction},{LRT},{MT_class},{electron_path}",file=fp_out)
+            print(MT_class)
 
 def count_elements(mol):
     """
