11f3637d299a1a238b63b08dda617f0dd357f056
ddd <542260219@qq.com>
2024-10-29 14:34:12 +0800

    修改logging.py文件名，不宜和python模块名相同， python /root/HiPRGen/HiPRGen/test.py 执行这类脚本import rdkit会有问题

--- MechFinder_rxn_filter_study/main.py
+++ MechFinder_rxn_filter_study/main.py
@@ -25,7 +25,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s',
+    format='%(asctime)s %(levelname)s %(module)s: %(lineno)d %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
