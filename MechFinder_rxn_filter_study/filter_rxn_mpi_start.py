5e025acc20e598cd5c484c4e59c56cd2f3fb1cc4
ddd <542260219@qq.com>
2024-10-31 14:23:47 +0800

    dd

--- MechFinder_rxn_filter_study/filter_rxn_mpi_start.py
+++ MechFinder_rxn_filter_study/filter_rxn_mpi_start.py
@@ -21,7 +21,7 @@ subprocess.run(
         '-n',
         number_of_threads,
         'python',
-        '/root/HiPRGen/MechFinder_rxn_filter_study/filter_rxn.py',
+        '/root/HiPRGen/MechFinder_rxn_filter_study/filter_rxn_mpi.py',
         f"/root/HiPRGen/data/libe_and_fmol_0911_all/mol_entries.pickle",
         f"/root/HiPRGen/data/libe_and_fmol_0911_all/rn.sqlite",
         f"rn_filtered.sqlite"
