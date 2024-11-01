d586770e7e0588c41c489787f26ebca4df2cfc5b
ddd <542260219@qq.com>
2024-11-01 11:26:07 +0800

    dd

--- MechFinder_rxn_filter_study/filter_rxn_mpi_bohrium_start.py
+++ MechFinder_rxn_filter_study/filter_rxn_mpi_bohrium_start.py
@@ -17,8 +17,9 @@ def split_rxn_db(original_rxn_db_path,split_num,output_dir):
     ori_conn = sqlite3.connect(original_rxn_db_path)
     ori_cursor = ori_conn.cursor()
     # 计算原表的行数
-    ori_cursor.execute(f"SELECT COUNT(*) FROM reactions")
-    total_rows = ori_cursor.fetchone()[0]
+    # ori_cursor.execute(f"SELECT COUNT(*) FROM reactions")
+    # total_rows = ori_cursor.fetchone()[0]
+    total_rows = 956946513
     print(f"Total number of rows in original table: {total_rows}")
     # 计算每个新表应该有多少行
     split_table_rows = total_rows // split_num
@@ -44,9 +45,7 @@ def split_rxn_db(original_rxn_db_path,split_num,output_dir):
                     dG                  REAL NOT NULL,
                     dG_barrier          REAL NOT NULL,
                     is_redox            INTEGER NOT NULL,
-                    template            STRING,
-                    mapped_rxn          STRING,
-                    rxn                 STRING
+                  
             );
         """)
         split_conn.commit()
@@ -57,7 +56,7 @@ def split_rxn_db(original_rxn_db_path,split_num,output_dir):
         print(orl_sql_str)
         ori_cursor.execute(orl_sql_str)
         for row in tqdm(ori_cursor):
-            split_cursor.execute("INSERT INTO reactions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+            split_cursor.execute("INSERT INTO reactions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
 
         split_conn.close()
         ori_offset += split_table_rows
