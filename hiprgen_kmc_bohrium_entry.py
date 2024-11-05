ef240eb08616af975108160fa16bbec3e8f8add2
ddd <542260219@qq.com>
2024-11-05 13:23:12 +0800

    dgl和  dp.launching.cli有冲突  去除dplaunching

--- hiprgen_kmc_bohrium_entry.py
+++ hiprgen_kmc_bohrium_entry.py
@@ -3,105 +3,72 @@ import shutil
 from pathlib import Path
 
 from HiPRGen.launching_entry import run_with_id, run_with_smiles
-from dp.launching.cli import to_runner, SubParser, default_minimal_exception_handler, run_sp_and_exit
-from dp.launching.typing import BaseModel, Field
-from dp.launching.typing import InputFilePath, OutputDirectory, MinMaxRange
-from dp.launching.typing import Int, Float, List, Enum, String, Set, Union, Literal, Optional
-from dp.launching.typing.io import InputMoleculeContent
 
+#
+# class Subsidiary_Molecule_Options(String, Enum):
+#     """
+#     Define the target to be predicted.
+#     """
+#     Li = 'Li+1'
+#     OH = 'OH-1'
+#     C2H4 = 'C2H4'
+#     H2O = 'H2O'
+#     H2 = 'H2'
+#     CO = 'CO'
+#     CO2 = 'CO2'
+#     LiF = 'LiF'
+#     nll = 'None'
+#     LiCO3 = 'LiCO3-1'
+#     H = 'H+1'
+#     Li2CO3 = 'Li2CO3+1'
 
-class Subsidiary_Molecule_Options(String, Enum):
-    """
-    Define the target to be predicted.
-    """
-    Li = 'Li+1'
-    OH = 'OH-1'
-    C2H4 = 'C2H4'
-    H2O = 'H2O'
-    H2 = 'H2'
-    CO = 'CO'
-    CO2 = 'CO2'
-    LiF = 'LiF'
-    nll = 'None'
-    LiCO3 = 'LiCO3-1'
-    H = 'H+1'
-    Li2CO3 = 'Li2CO3+1'
-
-
-class SMILES(BaseModel):
-    type: Literal['SMILES']
-    main_mol: Optional[InputMoleculeContent] = Field(default='CCOC(=O)OCC', content_type="smiles", description="Input primary molecule")
-    sub_mol: Subsidiary_Molecule_Options = Field(..., description='Input subsidiary molecule')
-    n_sim: Int = Field(default=1000, description='Number of simulations for kMC')
-    database_dir: String = Field( description='Path to the database')
-
-def SMILES_task(opts: SMILES, output_directory):
-    database_paths = {
-        'rn_db_path': rf'{opts.database_dir}/rn.sqlite',
-        'mol_entry_file_path': rf'{opts.database_dir}/mol_entries.pickle',
-        'mol_picture_folder_path': rf'{opts.database_dir}/mol_pictures',
-    }
-    run_with_smiles(
-        simulation_times=opts.n_sim,
-        num_cores=8,
-        main_mol_smiles=opts.main_mol.get_value(),
-        sub_mol_name=opts.sub_mol.value,
-        output_dir=output_directory,
-        default_file_paths=database_paths
-    )
-
-
-class ID(BaseModel):
-    type: Literal['ID']
-    main_mol_id: Int = Field(default=10442, description='Input ID for the main molecule')
-    sub_mol_ids: List = Field(default=5253, description='Input IDs for the subsidiary molecule')
-    n_sim: Int = Field(default=1000, description='Number of simulations for kMC')
-    database_dir: String = Field( description='Path to the database')
-
-
-def ID_task(opts: ID, output_directory):
-    database_paths = {
-        'rn_db_path': rf'{opts.database_dir}/rn.sqlite',
-        'mol_entry_file_path': rf'{opts.database_dir}/mol_entries.pickle',
-        'mol_picture_folder_path': rf'{opts.database_dir}/mol_pictures',
-    }
-    run_with_id(
-        main_mol_id=opts.main_mol_id,
-        sub_mol_ids=opts.sub_mol_ids,
-        simulation_times=opts.n_sim,
-        num_cores=8,
-        output_dir=output_directory,
-        default_file_paths=database_paths
-    )
-
-
-class kMC_pathfinding_Model(BaseModel):
-    Input_Format: Union[ID, SMILES] = Field(discriminator="type")
-    output_directory: OutputDirectory = Field(default='./output')
-
-
-def kMC_pathfinding_task(opts: kMC_pathfinding_Model):
-    if opts.Input_Format.type == 'ID':
-        ID_task(opts.Input_Format, opts.output_directory.get_full_path())
-    elif opts.Input_Format.type == 'SMILES':
-        SMILES_task(opts.Input_Format, opts.output_directory.get_full_path())
-    else:
-        raise NotImplementedError
-
-
-def to_parser():
-    return {
-        "kMC_pathfinding": SubParser(kMC_pathfinding_Model, kMC_pathfinding_task,
-                                     "Run kMC and generate pathways to possiable products."),
-    }
 
 
 if __name__ == '__main__':
     import sys
+    import json
+    #python /root/HiPRGen/hiprgen_kmc_bohrium_entry.py kMC_pathfinding --json-config app_param.json
+    if len(sys.argv) !=4:
+        print('Please run with kMC_pathfinding --json-config app_param.json')
+        exit()
+    if sys.argv[1] != 'kMC_pathfinding':
+        print('Please run with kMC_pathfinding')
+        exit()
 
-    run_sp_and_exit(
-        to_parser(),
-        description="Example",
-        version="0.1.0",
-        exception_handler=default_minimal_exception_handler,
+    if sys.argv[2] != '--json-config':
+        print('Please run with --json-config')
+        exit()
+
+    json_param_path = sys.argv[3]
+
+    # {
+    #     "output_dir": output_dir,
+    #     "Input_Format": {
+    #         "type": "ID",
+    #         "main_mol_id": 13589,
+    #         "sub_mol_ids": [3274, 14191, 145],
+    #         "n_sim": 1000,
+    #         "database_dir": "/root/HiPRGen/data/libe_and_fmol_0911_all_rn_filter",
+    #         # "/root/HiPRGen/data/new_libe_fmol_20240731",#"/root/HiPRGen/data/libe",#"/root/HiPRGen/data/new_libe_fmol_20240731",
+    #     },
+    # }
+    param_dict= json.load(open(json_param_path))
+
+    database_dir=param_dict["Input_Format"]["database_dir"]
+    main_mol_id=param_dict["Input_Format"]["main_mol_id"]
+    sub_mol_ids=param_dict["Input_Format"]["sub_mol_ids"]
+    n_sim=param_dict["Input_Format"]["n_sim"]
+    output_directory=param_dict["output_dir"]
+    database_paths = {
+        'rn_db_path': rf'{database_dir}/rn.sqlite',
+        'mol_entry_file_path': rf'{database_dir}/mol_entries.pickle',
+        'mol_picture_folder_path': rf'{database_dir}/mol_pictures',
+    }
+    run_with_id(
+        main_mol_id= main_mol_id ,
+        sub_mol_ids=sub_mol_ids,
+        simulation_times=n_sim,
+        num_cores=8,
+        output_dir=output_directory,
+        default_file_paths=database_paths
     )
